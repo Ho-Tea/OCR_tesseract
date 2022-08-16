a21ce6806c7f408d6208b6772d605f49367c91de
yoonjuho <dbswn990@naver.com>
2022-08-17 03:52:21 +0900

    feat: text출력

--- test_ocr4.py
+++ test_ocr4.py
@@ -46,7 +46,7 @@ def plt_imshow(title='image', img=None, figsize=(8, 5)):
         plt.show()
 
 
-def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=400):
+def make_scan_image(image, width, ksize=(5,5), min_threshold=50, max_threshold=200):
   image_list_title = []
   image_list = []
 
@@ -64,8 +64,8 @@ def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=
   binary = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,15,2)
   
   # 순서
-  image_list_title = [ 'gray','blurred','binary']
-  image_list = [gray, blurred, binary]
+  #image_list_title = [ 'gray','blurred','binary']
+  #image_list = [gray, blurred, binary]
 
   # contours를 찾아 크기순으로 정렬
   cnts = cv2.findContours(binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -99,47 +99,45 @@ def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=
   cv2.drawContours(output, [findCnt], -1, (0, 255, 0), 2)
   image_list_title.append("Outline")
   image_list.append(output)
-  plt_imshow(image_list_title, image_list)
+  #plt_imshow(image_list_title, image_list)
   
   # 원본 이미지에 찾은 윤곽을 기준으로 이미지를 보정
   receipt = four_point_transform(org_image, findCnt.reshape(4, 2) * ratio)
  
-  plt_imshow(image_list_title, image_list)
-  plt_imshow("Receipt Transform", receipt)
+  #plt_imshow(image_list_title, image_list)
+  #plt_imshow("Receipt Transform", receipt)
   
-  #원하는 영역 추출로 변환
+  #원하는 영역 추출로 변환과정
+  #grayscale로 변환
   gray = cv2.cvtColor(receipt, cv2.COLOR_BGR2GRAY)
   (H, W) = gray.shape
-  
- # getStructuringElement -> 형태학적 변환 인자로 커널사이즈 조정
-  rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30,17))
-  sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 21))
- 
-  gray = cv2.GaussianBlur(gray, (11, 11), 0)
+
+  # getStructuringElement -> 형태학적 변환 인자로 커널사이즈 조정
+  #이미지 필터링과정
+  rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30,10))
+  sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
+  gray = cv2.GaussianBlur(gray, (15, 15), 0)
   blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
- 
-  grad = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+  grad = cv2.Sobel(blackhat, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=-1)
   grad = np.absolute(grad)
   (minVal, maxVal) = (np.min(grad), np.max(grad))
   grad = (grad - minVal) / (maxVal - minVal)
   grad = (grad * 255).astype("uint8")
-
  #close
-  #grad = cv2.morphologyEx(grad, cv2.MORPH_OPEN, rectKernel)
+  grad = cv2.morphologyEx(grad, cv2.MORPH_CLOSE, rectKernel)
   thresh = cv2.threshold(grad, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
- 
- #open + 침식
+ #close + Dilation(팽창) + Erosion(침식)
   close_thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
-  close_thresh = cv2.dilate(close_thresh,None, iterations=3)
   close_thresh = cv2.erode(close_thresh, None, iterations=3)
+  close_thresh = cv2.dilate(close_thresh,None, iterations=3)
   
   
- 
-  
-  plt_imshow(["Original", "Blackhat", "Gradient", "Rect Close", "Square Close"], [receipt, blackhat, grad, thresh, close_thresh], figsize=(16, 10))
+  #이미지의 형태를 변경시켜 노이즈 제거 및 contour 적용 경계찾기
+  #plt_imshow(["Original", "Blackhat", "Gradient", "Rect Close", "Square Close"], [receipt, blackhat, grad, thresh, close_thresh], figsize=(16, 10))
 
 
-  #Detection - 여기부분만 손대서 윤곽선의 특정부분만을 catch하면 될듯 그리고 merge
+  #Detection
+  #contour 찾기
   cnts = cv2.findContours(close_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
   cnts = imutils.grab_contours(cnts)
   cnts = sort_contours(cnts, method="top-to-bottom")[0]
@@ -147,23 +145,24 @@ def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=
   roi_list = []
   roi_title_list = []
  
-  margin = 20
+  margin = 50
   receipt_grouping = receipt.copy()
  
   for c in cnts:
     (x, y, w, h) = cv2.boundingRect(c)
     ar = w // float(h)
- 
-    #if  ar:
-    color = (0, 255, 0)
-    roi = receipt[y - margin:y + h + margin, x - margin:x + w + margin]
-    if roi.size == 0:
-      continue
-    roi_list.append(roi)
-    roi_title_list.append("Roi_{}".format(len(roi_list)))
-    #else:
-     # color = (0, 0, 255)
- 
+    #cnts중 특정영역에 해당하는 부분만 따로 표시
+    if (w/2) > x and (h/4) < y and (3*h/4) < y:
+      color = (0, 255, 0)
+      roi = receipt[y-margin:y + h + margin, x:x + w + margin]
+      #크기가 0인경우 에러를 야기하므로 if문 처리로 회피
+      if roi.size == 0:
+        continue
+      roi_list.append(roi)
+      roi_title_list.append("Roi_{}".format(len(roi_list)))
+    else:
+      color = (0, 0, 255)
+    #보여지는 이미지 상 그리기
     cv2.rectangle(receipt_grouping, (x - margin, y - margin), (x + w + margin, y + h + margin), color, 2)
     cv2.putText(receipt_grouping, "".join(str(ar)), (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.65, color, 2)
   
@@ -173,7 +172,8 @@ def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=
   print("[INFO] OCR결과:")
   
   plt_imshow(roi_title_list, roi_list, figsize=(16, 10))
-
+  
+  #텍스트 출력
   for roi in roi_list: 
     #roi_list는 좌표값을 의미한다
     gray_roi= cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
@@ -182,14 +182,14 @@ def make_scan_image(image, width, ksize=(5,5), min_threshold=400, max_threshold=
     if isHangul(roi_text):
       print(roi_text)
 
-
+#한글을 check하는 부분
 def isHangul(text):
     #Check the Python Version
     hanCount = len(re.findall(u'[\u3130-\u318F\uAC00-\uD7A3]+', text))
     return hanCount > 0
 
 
-url = 'https://img.etnews.com/photonews/1707/971120_20170705143932_354_0001.jpg'
+url = 'https://blog.kakaocdn.net/dn/q8bkq/btqJCt61orM/J4OuH0kgcyyVkPL8e76WD0/img.jpg'
  
 image_nparray = np.asarray(bytearray(requests.get(url).content), dtype=np.uint8)
 org_image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR) 
